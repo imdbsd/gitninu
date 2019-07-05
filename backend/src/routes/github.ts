f75bb5de35d11e49b04efe8d470ca1db6564b271
imdbsd <budisuryadarma0407@gmail.com>
2019-07-05 23:53:45 +0700

    adding some test route to github

--- backend/src/routes/github.ts
+++ backend/src/routes/github.ts
@@ -4,4 +4,28 @@ const router: Router = Router()
 
 router.get('/', (req, res) => res.status(200).json({'message': 'hello fellas'}))
 
+router.get('/repository', (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: 'please specify owner and repo name'
+    })
+})
+
+router.get('/repository/:owner', (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: 'please specify repo name'
+    })
+})
+
+router.get('/repository/:owner/:repo', (req, res) => {
+    console.log(req.params)
+    res.status(200).json({
+        success: true,
+        message: 'uwu'
+    })
+})
+
+
+
 export default router
