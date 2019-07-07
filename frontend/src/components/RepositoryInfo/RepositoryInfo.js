92710995500fd1cb43d5c0c290b4c699f06ab909
imdbsd <budisuryadarma0407@gmail.com>
2019-07-07 15:35:18 +0700

    adding style for repository info

--- frontend/src/components/RepositoryInfo/RepositoryInfo.js
+++ frontend/src/components/RepositoryInfo/RepositoryInfo.js
@@ -3,12 +3,14 @@ import React from 'react'
 function RepositoryInfo(props) {
     const { info } = props
     return (
-        <div>
-            <div>
+        <section className="repo-info">
+            <div className="repo-info__avatar">
                 <img src="https://avatars2.githubusercontent.com/u/16449020?v=4" />
             </div>
-            <p>{info.owner.login}</p>
-        </div>
+            <h1 className="repo-info__name">{info.name}</h1>
+            <p className="repo-info__owner">by {info.owner.login}</p>
+            <p className="repo-info__total-issues">Total Issues: <span className="has-text-info">{info.totalIssues.totalCount}</span></p>
+        </section>
     )
 }
 
