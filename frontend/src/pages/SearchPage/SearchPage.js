9aa205ce30571a9c7303c3bb649ef2344be9fada
imdbsd <budisuryadarma0407@gmail.com>
2019-07-07 20:06:42 +0700

    remove dummy data

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -10,14 +10,7 @@ import {
 export default class SearchPage extends Component {
     state = {
         openForm: false,
-        searchResult: {
-            id: "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==",
-            name: "react",
-            openIssues: {totalCount: 572},
-            owner: {login: "facebook", avatarUrl: "https://avatars3.githubusercontent.com/u/69631?v=4"},
-            totalIssues: {totalCount: 7646},
-            closedIssues: {totalCount: 7074}
-        }, 
+        searchResult: {}, 
         errorFind: null
     }
 
