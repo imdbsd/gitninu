e85513cefefd72f1b09748b683b0f449d4738914
imdbsd <budisuryadarma0407@gmail.com>
2019-07-07 12:40:34 +0700

    remove unused file and add SearchForm components

--- frontend/src/components/SearchForm/SearchForm.js
+++ frontend/src/components/SearchForm/SearchForm.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react'
 export default class SearchForm extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <form className="form-search">
+                <div className="field">
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Username / Organization" />
+                    </div>
+                </div>
+                <div className="field">
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Repository" />
+                    </div>
+                </div>
+                <div className="control">
+                    <button className="button is-primary">Find</button>
+                </div>
+            </form>
         )
     }
 }
